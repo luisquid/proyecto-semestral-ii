127ca0feffcc51cd60b4b260a930160fe924f482
GalenKishi <galenarturo@gmail.com>
2020-11-27 07:28:57 -0600

    MovePlayer con Botones
    Dos botones que mueven al objeto de izquierda a derecha

---
+++ Assets/_Kishi/Scripts/MovePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float moveNegative;
+	public float movePositive;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    
+    }
+    
+	public void MovePlayerLeft()
+	{
+		rb.velocity = new Vector2(moveNegative, 0);
+	}
+	
+	public void MovePlayerRight()
+	{
+		rb.velocity = new Vector2(movePositive, 0);
+	}
+}
+
