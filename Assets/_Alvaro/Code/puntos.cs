79a42b1b0a8a6f42d71a50fc091fa5b7ad304489
alvarofenix1158 <62984053+alvarofenix1158@users.noreply.github.com>
2020-11-27 08:28:31 -0600

    Puntos
    Contador de puntos y tiempo.
    Te amo bb.

---
+++ Assets/_Alvaro/Code/puntos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class puntos : MonoBehaviour
+{
+    private TextMeshProUGUI Marcadordepuntos;
+    public  TextMeshProUGUI timeloco;
+    public GameObject holis;
+    int Puntoslocos = 0;
+    float Tiempoloco;
+    int tiempoanterior;
+
+    void Start()
+    {
+        Marcadordepuntos = GetComponent<TextMeshProUGUI>();
+        Tiempoloco += Time.deltaTime;
+        mostrarTiempo();
+    }
+
+    void Update()
+    {
+        Tiempoloco += Time.deltaTime;
+        int tiemponuevo = (int)Tiempoloco;
+
+        Marcadordepuntos.text = Puntoslocos.ToString();
+
+        if (tiemponuevo != tiempoanterior) 
+        {
+
+            timeloco.text = tiemponuevo.ToString();
+            tiempoanterior = tiemponuevo;
+            Puntoslocos = Puntoslocos + 5;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        { 
+        
+
+        
+        }
+
+           
+    }
+    void mostrarTiempo()
+    {
+        GetComponent<TextMeshProUGUI>().text = "TIME\n" + ((int)Tiempoloco).ToString();
+    }
+}
---
+++ Assets/_Alvaro/Code/tiempo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class tiempo : MonoBehaviour
+{
+    float Tiempoloco;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+        Tiempoloco += Time.deltaTime;
+        mostrarTiempo();
+        
+    }
+
+    void mostrarTiempo()
+    {
+        GetComponent<TextMeshProUGUI>().text = "TIME\n" + ((int)Tiempoloco).ToString();
+    }
+
+
+}
