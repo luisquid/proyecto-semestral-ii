bc04a3798c18287295c4ea25d884164156009c4f
GalenKishi <galenarturo@gmail.com>
2020-12-04 08:58:47 -0600

    Puntos, y Flujo de pantalla
    -Obtener los puntos en store
    -Menu
    -Game
    -Credits

---
+++ Assets/_Kishi/Scripts/Escenes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	public void abrirEscenaPlay() {
+		SceneManager.LoadScene("Game");
+	}
+	
+	public void abrirCredits (){
+		SceneManager.LoadScene("Credits");
+	}
+	
+	public void abrirStore (){
+		SceneManager.LoadScene("Store");
+	}
+	
+	public void abrirMenu (){
+		SceneManager.LoadScene("Menu");
+	}
+}
---
+++ Assets/_Kishi/Scripts/ScripStore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScripStore : MonoBehaviour
+{
+	public Text textPointStore;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    textPointStore.text = PlayerPrefs.GetInt("Puntos").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
