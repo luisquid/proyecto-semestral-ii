68e7e3645d58f02cd3a8135ee33860730fae6f01
GalenKishi <galenarturo@gmail.com>
2020-12-07 10:47:15 -0600

    Store y Puntos

---
+++ Assets/_Kishi/Scripts/ShopItemDetails.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopItemDetails : MonoBehaviour
+{
+	public string itemName;
+	public int itemCost;
+	public bool comprado;
+	private ScripStore scriptStore;
+    // Start is called before the first frame update
+    void Start()
+	{
+		scriptStore = FindObjectOfType<ScripStore>();
+	    GetComponentsInChildren<TextMeshProUGUI>()[0].text = itemName;
+	    GetComponentsInChildren<TextMeshProUGUI>()[1].text = itemCost + "";
+	    comprado = PlayerPrefs.GetInt(itemName) == 1 ? true : false;
+	    if (comprado){
+	    	GetComponentInChildren<Button>().interactable = false;
+	    }
+	    // GetComponentInChildren<Button>().onClick.AddListener(() => TestAction());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space)){
+	    	PlayerPrefs.SetInt(itemName, 0);
+	    }
+    }
+	public void ItemComprado(){
+		if(itemCost <= PlayerPrefs.GetInt("Puntos")){
+			PlayerPrefs.SetInt(itemName, 1);
+			GetComponentInChildren<Button>().interactable = false;
+			PlayerPrefs.SetInt("Puntos", PlayerPrefs.GetInt("Puntos") -itemCost);
+			scriptStore.textPointStore.text = PlayerPrefs.GetInt("Puntos") + "";
+		}
+		else{
+			Debug.Log("No hay lana joven");
+		}
+	}
+}
